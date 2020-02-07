862c7dd6751d007142e9421d673e63952d097125
DanikLebedev <danik_lebedev1999@mail.ru>
2020-02-07 20:13:57 +0300

    Add logout button

--- src/components/AdminNavigation/AdminNavigation.tsx
+++ src/components/AdminNavigation/AdminNavigation.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { Nav } from 'react-bootstrap'
-import {NavLink} from "react-router-dom";
+import {NavLink, useHistory} from "react-router-dom";
+import {AuthContext} from "../../context/auth.context";
+
 
 export const AdminNavigation = () => {
+    const auth = useContext(AuthContext)
+    const history = useHistory()
+    const logoutHandler = (event: { preventDefault: () => void; }) => {
+        event.preventDefault()
+        auth.logout()
+        history.push('/')
+    }
+
     return (
         <Nav className="justify-content-center" activeKey="/home">
             <Nav.Item className={'mr-2'}>
@@ -10,6 +20,11 @@ export const AdminNavigation = () => {
             </Nav.Item>
             <Nav.Item>
                 <NavLink to='/admin/create'>Add content</NavLink>
+            </Nav.Item>
+            <Nav.Item>
+                <NavLink onClick={logoutHandler} to='/'>
+                    Logout
+                </NavLink>
             </Nav.Item>
         </Nav>
     )
--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
 import '../../assets/rglstyles.css'
 import '../../assets/resizablestyles.css'
 import {AdminNavigation} from "../../components/AdminNavigation/AdminNavigation";
@@ -17,6 +17,7 @@ export const AdminPage: React.FC = () => {
             <Route path='/admin/create' exact>
                 <AdminPageCreate/>
             </Route>
+            <Redirect to={'/admin/info'}/>
         </Switch>
     )
 
